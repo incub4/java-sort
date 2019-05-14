2216f024e3e4f950d66c102c3a1754cca5765eb6
Vladimir <cdkey2008@yandex.ru>
2019-05-14 21:47:06 +0300

    test

--- src/MySnake.java
+++ src/MySnake.java
@@ -5,27 +5,27 @@ import Sort.SelectionSort;
 
 public class MySnake {
     public static void main(String[] args) {
-        SelectionSort ss = new SelectionSort(); // Экз-р класса с методами сортировки
         int [] arrey = {4, 9,-3, 8, 1, 10,-50, 5, 120, -120}; // То что будем сортировать
         int [] arrB = arrey.clone();
         int [] arrC = arrey.clone();
         int [] arrD = arrey.clone();
 
-//        ss.selectionSort(arrey); // Сортировка
-//        ss.arrPrint(arrey); // Вывод в консоль отсортированного массива
+        System.out.println("SelectionSort:");
+        SelectionSort.selectionSort(arrey); // Сортировка
+        SelectionSort.arrPrint(arrey); // Вывод в консоль отсортированного массива
 
-//        BubbleSort bs = new BubbleSort();
-//        bs.bublesort(arrB);
-//        ss.arrPrint(arrB);
+        System.out.println("\nBubbleSort:");
+        BubbleSort.bublesort(arrB);
+        SelectionSort.arrPrint(arrB);
 
-//        CocktailSort cs = new CocktailSort();
-//        cs.cocktailsort(arrC);
-//        ss.arrPrint(arrC);
+        System.out.println("\nCocktailSort:");
+        CocktailSort.cocktailsort(arrC);
+        SelectionSort.arrPrint(arrC);
 
-        DoubleSelectionSort dss = new DoubleSelectionSort();
-        dss.doubleselectionsort(arrD);
-        ss.arrPrint(arrD);
-        System.out.println("hello world");
+        System.out.println("\nDoubleSelectionSort:");
+        DoubleSelectionSort.doubleselectionsort(arrD);
+        SelectionSort.arrPrint(arrD);
+
     }
 
 }
