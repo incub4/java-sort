b2bc37a21f782e85896e5403926dddec28bcd64a
vladimir <cdkey2008@yandex.ru>
2019-05-14 12:01:01 -0700

    сделал все ф-ии Статик

--- src/Sort/DoubleSelectionSort.java
+++ src/Sort/DoubleSelectionSort.java
@@ -1,9 +1,7 @@
 package Sort;
 
 public class DoubleSelectionSort {
-    public  void doubleselectionsort(int [] arrey){
-        BubbleSort bs = new BubbleSort();
-
+    public static void doubleselectionsort(int [] arrey){
         for (int firstEl=0, lastEl=arrey.length; firstEl!=lastEl; firstEl++, lastEl--) {
             int minEl=firstEl;
             int maxEl=firstEl;
@@ -13,13 +11,13 @@ public class DoubleSelectionSort {
                 if (arrey[i] < arrey[minEl])
                     minEl = i;
             }
-            bs.swap(arrey, maxEl, lastEl-1);
+            BubbleSort.swap(arrey, maxEl, lastEl-1);
             if ((lastEl-1)==minEl) {
                 minEl = maxEl;
                 maxEl = lastEl-1;
             }else maxEl=lastEl-1;
 
-            bs.swap(arrey, minEl, firstEl);
+            BubbleSort.swap(arrey, minEl, firstEl);
         }
     }
 }
--- src/Sort/SelectionSort.java
+++ src/Sort/SelectionSort.java
@@ -4,7 +4,7 @@ package Sort;
 // Класс содержащий методы Сортировки выбором и печати массиа в консоль.
 
 public class SelectionSort { // Класс с методом который должен сортировать
-    public  void selectionSort(int [] arrey){
+    public static void selectionSort(int [] arrey){
         for (int k=arrey.length-1; k>=0; k--) {
             int max=0; // Индекс максимального элемента в неотсортированном массиве
             for (int i = 0; i < k; i++) { // Поиск максимального элемента в неотсортированном массиве
@@ -12,12 +12,11 @@ public class SelectionSort { // Класс с методом который до
                     max = i + 1;
             }
             // Меняем местами максимальный элемент в неотсортированном массиве с последним его эл-м
-            BubbleSort bs = new BubbleSort();
-            bs.swap(arrey, max, k);
+            BubbleSort.swap(arrey, max, k);
         }
     }
 
-    public void arrPrint(int [] arrey){
+    public static void arrPrint(int [] arrey){
         for (int i=0; i<arrey.length; i++)
         {
             System.out.print(arrey[i]+" ");
